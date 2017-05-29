f1e6c2179b18d6dba57fd3a58814d38a5292ba2b
Kilian <kilianmb92@gmail.com>
2017-05-29 18:59:37 +0200

    index of users

--- src/AppBundle/Repository/UserRepository.php
+++ src/AppBundle/Repository/UserRepository.php
@@ -2,9 +2,36 @@
 
 namespace AppBundle\Repository;
 
+use AppBundle\Entity\User;
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\QueryBuilder;
 
 class UserRepository extends EntityRepository
 {
+    public function findUsersWithUsernameIn(array $usernames = [])
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder();
+        $query = $qb->select('u')->from('AppBundle\Entity\User', 'u');
 
+        $queryOfUsersByUsernamesInArray = $this->queryUsersWithUsernameIn($query, $usernames);
+
+        $query = $query->where($queryOfUsersByUsernamesInArray);
+
+        return $query->getQuery()->getResult();
+    }
+
+    private function queryUsersWithUsernameIn(QueryBuilder $qb, array $usernames = [])
+    {
+        if (!$qb) {
+            $qb = $this->getEntityManager()->createQueryBuilder();
+        }
+        $desiredUsernamesQueries = [];
+
+        foreach ($usernames as $username) {
+            $desiredUsernamesQueries[] = $qb->expr()->like('u.username', ':'.$username);
+            $qb->setParameter($username, '%'.$username.'%');
+        }
+
+        return $qb->expr()->orX()->addMultiple($desiredUsernamesQueries); 
+    }
 }
