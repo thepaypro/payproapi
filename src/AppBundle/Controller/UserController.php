1beeaee2a78bd785f28564f91e6a15972d8cd8a1
Juanma <juanma@mondeapp.com>
2017-06-21 11:22:37 +0200

    change password endpoint - added respect/validation (composer update)

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -10,6 +10,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Security\Core\User\UserInterface;
 use AppBundle\Controller\Traits\JWTResponseControllerTrait;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Respect\Validation\Validator as v;
+use Respect\Validation\Exceptions\NestedValidationException;
 
 /**
  * User controller.
@@ -21,10 +24,59 @@ class UserController extends Controller
     use JWTResponseControllerTrait;
 
     /**
+     * Check the user password
+     * @param  UserInterface $user
+     * @param  Request       $request
+     * @return JsonResponse
+     *
+     * @Security("has_role('ROLE_USER')")
+     * @Route("/update-password", name="update_password")
+     * @Method("PUT")
+     */
+    public function updatePasswordAction(UserInterface $user, Request $request) : JsonResponse
+    {
+        $old_password = $request->query->get('old_password');
+        $new_password = $request->query->get('new_password');
+        $confirm_password = $request->query->get('confirm_password');
+
+        $encoderService = $this->container->get('security.password_encoder');
+        $match = $encoderService->isPasswordValid($user, $old_password);
+        if(!$match) {
+            return $this->json([
+                'message' => 'Wrong old password',
+            ], 400);
+        }
+
+        $passwordValidator = v::notBlank()
+            ->Length(6, 6)
+            ->Digit();
+        try {
+            $passwordValidator->setName('Old password')->assert($old_password);
+            $passwordValidator->setName('New password')->assert($new_password);
+            $passwordValidator->equals($new_password)->setName('Confirm password')
+                ->assert($confirm_password);
+        } catch(NestedValidationException $exception) {
+            return $this->json([
+                'message' => $exception->getMessages()[0]
+            ], 400);
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $hash = $encoderService->encodePassword($user, $new_password);
+        $user->setPassword($hash);
+        $em->flush();
+
+        return $this->json([
+            'message' => 'Password change success',
+            'user' => $user,
+        ]);
+    }
+
+    /*
      * Returns if a user with the given phone number exists
      * @param  Request       $request
      * @return JsonResponse
-     * 
+     *
      * @Route("/check/{phoneNumber}", name="user_check")
      * @Method("GET")
      */
@@ -35,7 +87,7 @@ class UserController extends Controller
         $userRepository = $this->getDoctrine()->getRepository('AppBundle:User');
         $user = $userRepository->findOneByUsername($phoneNumber);
 
-        $data = ['isUser' => $user ? true : false]; 
+        $data = ['isUser' => $user ? true : false];
 
         return $this->json($data);
     }
@@ -45,7 +97,7 @@ class UserController extends Controller
      * @param  UserInterface $user
      * @param  Request       $request
      * @return JsonResponse
-     * 
+     *
      * @Route("/{id}", name="user_show")
      * @Method("GET")
      */
@@ -64,7 +116,7 @@ class UserController extends Controller
      * @param  UserInterface $user
      * @param  Request       $request
      * @return JsonResponse
-     * 
+     *
      * @Security("has_role('ROLE_USER')")
      * @Route("", name="users_list")
      * @Method("GET")
@@ -84,7 +136,7 @@ class UserController extends Controller
      * @param  UserInterface $user
      * @param  Request       $request
      * @return JsonResponse
-     * 
+     *
      * @Security("has_role('ROLE_USER')")
      * @Route("/{id}", name="user_update")
      * @Method("PUT")
