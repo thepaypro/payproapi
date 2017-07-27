ff3e13314e1e59d950bf4a4afc1560c726731055
kilian <kilianmb92@gmail.com>
2017-07-27 14:33:21 +0000

    more style fixing

--- src/AppBundle/Service/Contact/ContactListService.php
+++ src/AppBundle/Service/Contact/ContactListService.php
@@ -36,10 +36,10 @@ class ContactListService
     /**
      * This method will create a list with contacts.
      * 
-     * @param  Array $phoneNumbers
-     * @return Array
+     * @param  array $phoneNumbers
+     * @return array
      */
-    public function createList(string $userPhoneNumber, Array $phoneNumbers) : Array
+    public function createList(string $userPhoneNumber, array $phoneNumbers) : array
     {
         $userPhoneNumberObject = $this->phoneUtil->parse($userPhoneNumber, null);
         $contactsList = [];
