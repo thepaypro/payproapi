4534d01c0d389d09b0046e46216027ad7b317ae6
Adrià Batlle Cuito <sudobat@gmail.com>
2017-08-03 21:03:04 +0200

    Solved problems with annotations

--- src/AppBundle/Controller/CardController.php
+++ src/AppBundle/Controller/CardController.php
@@ -4,6 +4,9 @@ namespace AppBundle\Controller;
 
 use AppBundle\Controller\Traits\JWTResponseControllerTrait;
 use AppBundle\Exception\PayProException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
