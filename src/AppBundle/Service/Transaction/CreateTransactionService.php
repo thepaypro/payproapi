808bbf2bd08e6d461d060670a2a7a01a78dc4977
kili <kilianmb92@gmail.com>
2017-08-03 14:19:04 +0000

    validated funds when creating transaction

--- src/AppBundle/Service/Transaction/CreateTransactionService.php
+++ src/AppBundle/Service/Transaction/CreateTransactionService.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Service\Transaction;
 
+use AppBundle\Service\Balance\GetBalanceService;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 use AppBundle\Entity\Transaction;
@@ -20,35 +21,42 @@ class CreateTransactionService
     protected $accountRepository;
     protected $userRepository;
     protected $validationService;
+    protected $getBalanceService;
     protected $contisTransactionApiClient;
 
     /**
-     * @param TransactionRepository      $transactionRepository
-     * @param ValidatorInterface         $validationService
-     * @param ContisTransactionApiClient $contisAccountApiClient
+     * @param TransactionRepository $transactionRepository
+     * @param AccountRepository $accountRepository
+     * @param UserRepository $userRepository
+     * @param ValidatorInterface $validationService
+     * @param GetBalanceService $getBalanceService
+     * @param ContisTransactionApiClient $contisTransactionApiClient
      */
     public function __construct(
         TransactionRepository $transactionRepository,
         AccountRepository $accountRepository,
         UserRepository $userRepository,
         ValidatorInterface $validationService,
+        GetBalanceService $getBalanceService,
         ContisTransactionApiClient $contisTransactionApiClient
     ) {
         $this->transactionRepository = $transactionRepository;
         $this->accountRepository = $accountRepository;
         $this->userRepository = $userRepository;
         $this->validationService = $validationService;
+        $this->getBalanceService = $getBalanceService;
         $this->contisTransactionApiClient = $contisTransactionApiClient;
     }
 
     /**
      * This method will retrieve all the transactions from the database and from Contis and will merge them.
      *
-     * @param  int          $userId
-     * @param  int          $beneficiaryId
-     * @param  int          $amount
-     * @param  string       $subject
-     * @return Transaction  $transaction
+     * @param  int $userId
+     * @param  int $beneficiaryId
+     * @param  float|int $amount
+     * @param  string $subject
+     * @return Transaction $transaction
+     * @throws PayProException
      */
     public function execute(
         int $userId,
@@ -64,6 +72,9 @@ class CreateTransactionService
         if (!$payer) {throw new PayProException('Account not found', 400);}
         if ($payer == $beneficiary) {throw new PayProException('Beneficary account and destination account can not be the same', 400);}
         if (!$beneficiary) {throw new PayProException('Beneficiary not found', 400);}
+        if ($amount > $this->getBalanceService->execute($userId)) {
+            throw new PayProException('Insufficient funds', 400);
+        }
 
         $transaction = new Transaction(
             $payer,
