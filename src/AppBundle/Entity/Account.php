aa6902492fc4051b954bb680433bd475dab29bc9
nhal <originhality@hotmail.com>
2017-08-10 18:51:20 +0200

    updated migrations.

--- src/AppBundle/Entity/Account.php
+++ src/AppBundle/Entity/Account.php
@@ -129,7 +129,7 @@ class Account implements \JsonSerializable
     protected $street;
 
     /**
-     * @ORM\Column(type="string")
+     * @ORM\Column(type="string", nullable=true)
      */
     protected $buildingNumber;
 
