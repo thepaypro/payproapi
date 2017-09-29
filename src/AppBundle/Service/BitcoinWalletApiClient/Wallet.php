b9a39e08c26962cc9236f098ab2d2a82ad2c60ad
kili <kilianmb92@gmail.com>
2017-09-29 14:05:03 +0000

    added env parameter for the docker compose path

--- src/AppBundle/Service/BitcoinWalletApiClient/Wallet.php
+++ src/AppBundle/Service/BitcoinWalletApiClient/Wallet.php
@@ -6,8 +6,6 @@ use AppBundle\Exception\PayProException;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use AppBundle\Service\BitcoinWalletApiClient\Interfaces\WalletInterface;
-use Exception;
-use GuzzleHttp\Client;
 
 /**
  * Class Wallet
@@ -15,13 +13,13 @@ use GuzzleHttp\Client;
  */
 class Wallet implements WalletInterface
 {
-    protected $httpClient;
     protected $bitcoinWalletRequestService;
+    protected $dockerComposePath;
 
-    public function __construct(RequestService $bitcoinWalletRequestService)
+    public function __construct(RequestService $bitcoinWalletRequestService, string $dockerComposePath)
     {
         $this->bitcoinWalletRequestService = $bitcoinWalletRequestService;
-        $this->httpClient = new Client();
+        $this->dockerComposePath = $dockerComposePath;
     }
 
     /**
@@ -34,7 +32,7 @@ class Wallet implements WalletInterface
     public function create(string $walletIdentification, string $tenant): bool
     {
         $tenant = str_replace(' ', '', $tenant);
-        $cmd = 'docker-compose -f /var/bitcoinWalletCLI/docker-compose.yml run node ';
+        $cmd = 'docker-compose -f '.$this->dockerComposePath.' run node ';
         $cmd = $cmd.'/var/www/bin/wallet create '.$tenant.'Wallet 1-1 '.$tenant.' -t -f /wallets/'.$walletIdentification.'.dat';
 
         $process = new Process($cmd);
@@ -42,7 +40,7 @@ class Wallet implements WalletInterface
         try {
             $process->mustRun();
         } catch (ProcessFailedException $e) {
-            throw PayProException('ERROR BitcoinApiClient,'.$e->getMessage(), 500);
+            throw PayProException('ERROR BitcoinApiClient, error creating wallet: '.$e->getMessage(), 500);
         }
 
         return true;
@@ -56,18 +54,6 @@ class Wallet implements WalletInterface
      */
     public function getOne(string $walletIdentification): array
     {
-        try {
-            $response = $this->bitcoinWalletRequestService->call(
-                'GET',
-                '/wallet',
-                [
-                    'filename' => $walletIdentification
-                ]
-            );
-        } catch (Exception $exception) {
-            throw new PayProException('Bitcoin Wallet service unavailable', 500);
-        }
 
-        return $response;
     }
 }
