8bb54a41a82cc55612087a5b3375d3d0e74e36b5
ottegu <>
2017-10-17 14:02:33 +0200

    Add logger into contis call services, not finished

--- src/AppBundle/Service/ContisApiClient/Balance.php
+++ src/AppBundle/Service/ContisApiClient/Balance.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Service\ContisApiClient;
 
+use Symfony\Bridge\Monolog\Logger;
 use AppBundle\Entity\Account;
 use AppBundle\Exception\PayProException;
 
@@ -14,21 +15,25 @@ class Balance
     protected $requestService;
     protected $hashingService;
     protected $authenticationService;
+    protected $logger;
 
     /**
      * @param RequestService $requestService
      * @param HashingService $hashingService
      * @param AuthenticationService $authenticationService
+     * @param Logger $logger
      */
     public function __construct(
         RequestService $requestService,
         HashingService $hashingService,
-        AuthenticationService $authenticationService
+        AuthenticationService $authenticationService,
+        Logger $logger
     )
     {
         $this->requestService = $requestService;
         $this->hashingService = $hashingService;
         $this->authenticationService = $authenticationService;
+        $this->logger = $logger;
     }
 
     public function get(Account $account)
@@ -56,6 +61,12 @@ class Balance
         if ($response['Account_GetBalanceResult']['Description'] == 'Success ') {
             return $response['Account_GetBalanceResult']['ResultObject']['AvailableBalance'];
         }
+
+        $this->logger->addCritical(
+            'Call Params: '.json_encode($params).' // Call Request Params: '.json_encode($requestParams).' // Response Service: '.json_encode($response),
+            ['Account_GetBalance','ContisApiClient']
+        );
+        
         throw new PayProException("Bad Request", 400);
     }
 }
--- src/AppBundle/Service/ContisApiClient/Card.php
+++ src/AppBundle/Service/ContisApiClient/Card.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Service\ContisApiClient;
 
+use Symfony\Bridge\Monolog\Logger;
 use AppBundle\Entity\Card as CardEntity;
 use AppBundle\Exception\PayProException;
 
@@ -14,21 +15,25 @@ class Card
     protected $requestService;
     protected $hashingService;
     protected $authenticationService;
+    protected $logger;
 
     /**
      * @param RequestService $requestService
      * @param HashingService $hashingService
      * @param AuthorizationService $authorizationService
+     * @param Logger $logger
      */
     public function __construct(
         RequestService $requestService,
         HashingService $hashingService,
-        AuthenticationService $authenticationService
+        AuthenticationService $authenticationService,
+        Logger $logger
     )
     {
         $this->requestService = $requestService;
         $this->hashingService = $hashingService;
         $this->authenticationService = $authenticationService;
+        $this->logger = $logger;
     }
 
     /**
@@ -59,6 +64,12 @@ class Card
         if ($response['Card_RequestResult']['Description'] == 'Success ') {
             return true;
         }
+
+        $this->logger->addCritical(
+            'Call Params: '.json_encode($params).' // Call Request Params: '.json_encode($requestParams).' // Response Service: '.json_encode($response),
+            ['Card_Request','ContisApiClient']
+        );
+
         throw new PayProException("Bad Request", 400);
     }
 
@@ -92,6 +103,12 @@ class Card
             $response['Card_GetActivationCodeResult']['ResultObject'] != null) {
             return $response['Card_GetActivationCodeResult']['ResultObject'];
         }
+
+        $this->logger->addCritical(
+            'Call Params: '.json_encode($params).' // Call Request Params: '.json_encode($requestParams).' // Response Service: '.json_encode($response),
+            ['Card_GetActivationCode','ContisApiClient']
+        );
+
         throw new PayProException("Bad Request", 400);
     }
 
@@ -125,6 +142,12 @@ class Card
         if ($response['Card_ActivateResult']['Description'] == 'Success ') {
             return true;
         }
+
+        $this->logger->addCritical(
+            'Call Params: '.json_encode($params).' // Call Request Params: '.json_encode($requestParams).' // Response Service: '.json_encode($response),
+            ['Card_Activate','ContisApiClient']
+        );
+
         throw new PayProException("Bad Request", 400);
     }
 
@@ -158,6 +181,12 @@ class Card
         if ($response['Card_ChangeStatusResult']['Description'] == 'Success ') {
             return true;
         }
+
+        $this->logger->addCritical(
+            'Call Params: '.json_encode($params).' // Call Request Params: '.json_encode($requestParams).' // Response Service: '.json_encode($response),
+            ['Card_ChangeStatus','ContisApiClient']
+        );
+
         throw new PayProException("Bad Request", 400);
     }
 
@@ -167,11 +196,12 @@ class Card
      * @param int $cvv2
      * @return bool
      */
-    public function retrivePin(CardEntity $card, int $cvv2) : bool
+    public function retrivePin(CardEntity $card, string $hashCardNumber, int $cvv2) : bool
     {
         $params = [
-            'CardID' => $card->getContisCardId(),
-            'SecurityCode' => $cvv2
+            'HashCardNumber' => $hashCardNumber,
+            'SecurityCode' => $cvv2,
+            'CardID' => $card->getContisCardId()
         ];
 
         $params['Token'] = $this->authenticationService->getAuthenticationToken();
@@ -186,10 +216,45 @@ class Card
         $requestParams = $this->hashingService->generateHashDataStringAndHash($requestParams);
 
         $response = $this->requestService->call('Card_RetrivePIN', $params, $requestParams);
-        
+
         if ($response['Card_RetrivePINResult']['Description'] == '000') {
             return $response['Card_RetrivePINResult']['ResultObject'];
         }
+        throw new PayProException("Bad Request", 400);
+    }
+
+
+    public function getInfo(CardEntity $card)
+    {
+        $params = [
+            'CardHolderID' => $card->getAccount()->getCardHolderId(),
+            'AccountNumber' => $card->getAccount()->getAccountNumber(),
+            'CardID' => $card->getContisCardId(),
+            'SortCode' => $card->getAccount()->getSortCode()
+        ];
+
+        $params['Token'] = $this->authenticationService->getAuthenticationToken();
+
+        $requestParams = [
+            'Token' => $params['Token'],
+            'ClientUniqueReferenceID' => strtotime('now'),
+            'SchemeCode' => 'PAYPRO'
+        ];
+
+        $params = $this->hashingService->generateHashDataStringAndHash($params);
+        $requestParams = $this->hashingService->generateHashDataStringAndHash($requestParams);
+
+        $response = $this->requestService->call('Card_Lookup_GetInfo', $params, $requestParams);
+
+        if ($response['Card_Lookup_GetInfoResult']['ResponseCode'] == '000') {
+            return $response['Card_Lookup_GetInfoResult']['ResultObject'][0];
+        }
+
+        $this->logger->addCritical(
+            'Call Params: '.json_encode($params).' // Call Request Params: '.json_encode($requestParams).' // Response Service: '.json_encode($response),
+            ['Card_Lookup_GetInfo','ContisApiClient']
+        );
+        
         throw new PayProException("Bad Request", 400);
     }
 }
