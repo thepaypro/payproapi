0c2035f20bc00515dbbd69ec2bd4abda0c25bdc9
Roger Baiget <rogerbaiget@Mac-mini-de-Roger.local>
2018-03-01 13:39:47 +0100

    twin functions

--- src/AppBundle/Repository/AirdropRepository.php
+++ src/AppBundle/Repository/AirdropRepository.php
@@ -4,5 +4,21 @@ namespace AppBundle\Repository;
 
 class AirdropRepository extends BaseEntityRepository
 {
-	
+	public function getNextBeneficiary(){
+
+		$em = $this->getEntityManager();
+		$query = $em->createQuery(
+		    'SELECT p
+		       FROM AppBundle:Airdrop p
+		      WHERE p.transactionHash IS NULL'
+		);
+
+		$query->setMaxResults(1);
+		
+
+		// $query = $em->createQuery('SELECT * FROM AppBundle\Entity\Airdrop WHERE transaction_hash IS NULL LIMIT 1');
+
+		return $query->getResult();
+
+	}
 }
--- src/AppBundle/Service/Airdrop/AirdropService.php
+++ src/AppBundle/Service/Airdrop/AirdropService.php
@@ -16,6 +16,9 @@ use PayPro\Ethereum\Exception\EthereumException;
 class AirdropService
 {
 	protected $airdropRepository;
+	protected $tokenContractAddress;
+	protected $client;
+	protected $accounts;
 
 	/**
      * AirdropService constructor.
@@ -24,6 +27,11 @@ class AirdropService
 	public function __construct(AirdropRepository $airdropRepository)
     {
         $this->airdropRepository = $airdropRepository;
+
+        $this->tokenContractAddress = new Address("0xf2beae25b23f0ccdd234410354cb42d08ed54981");
+        $this->client = new EthereumClientRPC("http://192.168.1.107:8545");
+	    $this->accounts = $this->client->eth()->accounts();
+	    $this->client->setupWallet($this->accounts[69]);
     }
 
 
@@ -31,24 +39,7 @@ class AirdropService
     {
     	try{
 
-	    	$airdropUsers = $this->airdropRepository->findAll();
-
-	    	$this->client = new EthereumClientRPC("http://192.168.1.107:8545");
-	    	$this->accounts = $this->client->eth()->accounts();
-	    	$this->client->setupWallet($this->accounts[69]);
-	    	$tokenContractAddress = new Address("0xf2beae25b23f0ccdd234410354cb42d08ed54981");
-
-	    	foreach ($airdropUsers as $airdropUser) {
-	    		usleep(500000);
-	    		if(is_null($airdropUser->getTransactionHash())){
-	    			$transactionHash = $this->client->ERC20Wallet()->sendERC20($tokenContractAddress, new Address(trim($airdropUser->getWalletAddr())), new ERC20BasicUnit($airdropUser->getPips(),18), null, null);
-
-	    			$airdropUser->setTransactionHash($transactionHash);
-
-	    			$this->airdropRepository->save($airdropUser);
-	    		}
-			}
-			return $airdropUsers;
+	    	return $this->getNextBeneficiary();
 
 		} catch (EthereumException $e){
             throw $e;
@@ -58,4 +49,31 @@ class AirdropService
             throw new EthereumException ($e->getMessage(),5000,500);
         }	
     }
+
+
+    private function getNextBeneficiary(){
+
+    	$nextBeneficiary = $this->airdropRepository->getNextBeneficiary()[0];
+
+    	if (!is_null($nextBeneficiary)){
+    		$this->sendTransaction($nextBeneficiary);
+    	}else{
+    		return "You rock!";
+    	}
+
+    }
+
+    private function sendTransaction(Airdrop $airdropUser){
+
+    	$transactionHash = $this->client->ERC20Wallet()->sendERC20($this->tokenContractAddress, new Address(trim($airdropUser->getWalletAddr())), new ERC20BasicUnit($airdropUser->getPips(),18), null, null);
+
+		$airdropUser->setTransactionHash($transactionHash);
+		$this->airdropRepository->save($airdropUser);
+
+		$this->getNextBeneficiary();
+
+    }
+
+
+
 }
